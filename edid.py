882e852c2258bee6bcf8faa4d127a8485427cab0
That-Sasha <sashajtremayne@gmail.com>
2025-07-27 20:45:33 +1000

    Refactor header class, added edidproperty decorator

--- edid.py
+++ edid.py
@@ -49,6 +49,59 @@ def simple_test(test_class, expected):
         print(f'Simple test success!\n{highlight_match}')
 
 
+class EdidPropertyValue:
+    def __init__(self, value, byte_val, byte_range):
+        self.value = value
+        self.as_bytes = byte_val
+        self._byte_range = byte_range
+
+    @property
+    def byte_range(self):
+        return self._byte_range
+
+
+class EdidProperty:
+
+    def __init__(self, getter, setter=None, byte_range=None, byte_converter = lambda x: x.to_bytes()):
+        self._getter = getter
+        self._setter = setter
+        self._byte_range = byte_range
+        self._byte_converter = byte_converter
+
+    def __set__(self, obj, value):
+        if self.setter is None:
+            raise AttributeError
+        self._setter(obj, value)
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self._getter is None:
+            raise AttributeError
+        value = self._getter(obj)
+        return EdidPropertyValue(value, self._byte_converter(value), self._byte_range)
+
+    def setter(self, setter):
+        return type(self)(self._getter, setter, self._byte_range, self._byte_converter)
+
+    def getter(self, getter):
+        return type(self)(getter, self._setter, self._byte_range, self._byte_converter)
+
+    def byte_converter(self, converter):
+        return type(self)(self._getter, self._setter, self._byte_range, converter)
+
+    def _byte_range_setter(self, value):
+        self._byte_range = value
+
+    # This is hacky and I don't like it, but it seems to work
+    @property
+    def byte_range():
+        pass
+
+    @byte_range.setter
+    def byte_range(self, value):
+        self._byte_range_setter(value)
+
 
 class Header():
 # 20 bytes total
@@ -79,167 +132,109 @@ class Header():
         self._manufacture_year = manufacture_year
         self._edid_version = edid_version
 
-    @property
+
+    @EdidProperty
     def manufacturer_id(self):
-        # read the wiki, this id encoding fucking sucks
-        class ManufacturerId:
-
-            def __init__(self, value):
-                self._value = value
-
-            @property
-            def value(self):
-                return self._value
-
-            @property
-            def bytes(self):
-                return int('0' + ''.join([format(ord(x) - 64, '05b') for x in self._value.upper()]),2).to_bytes(2)
-
-            @property
-            def byte_range(self):
-                return [8,10]
-
-        return ManufacturerId(self._manufacturer_id)
+        return self._manufacturer_id
 
     @manufacturer_id.setter
     def manufacturer_id(self, value):
         self._manufacturer_id = value
 
-    @property
+    @manufacturer_id.byte_converter
+    def manufacturer_id(value):
+        return int('0' + ''.join([format(ord(x) - 64, '05b') for x in value.upper()]),2).to_bytes(2)
+
+    manufacturer_id.byte_range = [8,10]
+
+    # ===============================================================================================
+
+    @EdidProperty
     def product_code(self):
-        class ProductCode:
-
-            def __init__(self, value):
-                self._value = value
-
-            @property
-            def value(self):
-                return self._value
-
-            @property
-            def bytes(self):
-                return bytes.fromhex(self._value)
-
-            @property
-            def byte_range(self):
-                return [10,12]
-
-        return ProductCode(self._product_code)
+        return self._product_code
 
     @product_code.setter
     def product_code(self, value):
         self._product_code = value
 
-    @property
+    @product_code.byte_converter
+    def product_code(value):
+        return bytes.fromhex(value)
+
+    product_code.byte_range = [10,12]
+
+    # ===============================================================================================
+
+    @EdidProperty
     def serial_num(self):
-        class SerialNum:
-
-            def __init__(self, value):
-                self._value = value
-
-            @property
-            def value(self):
-                return self._value
-
-            @property
-            def bytes(self):
-                return self._value.to_bytes(4)
-
-            @property
-            def byte_range(self):
-                return [12,16]
-
-        return SerialNum(self._serial_num)
+        return self._serial_num
 
     @serial_num.setter
     def serial_num(self, value):
         self._serial_num = value
 
-    @property
+    @serial_num.byte_converter
+    def serial_num(value):
+        return value.to_bytes(4)
+
+    product_code.byte_range = [12,16]
+
+    # ===============================================================================================
+
+    @EdidProperty
     def manufacture_week(self):
-        class ManufactureWeek:
-
-            def __init__(self, value):
-                self._value = value
-
-            @property
-            def value(self):
-                return self._value
-
-            @property
-            def bytes(self):
-                return self._value.to_bytes()
-
-            @property
-            def byte_range(self):
-                return [16,17]
-
-        return ManufactureWeek(self._manufacture_week)
+        return self._manufacture_week
 
     @manufacture_week.setter
     def manufacture_week(self, value):
         self._manufacture_week = value
 
-    @property
+    product_code.byte_range = [16,17]
+
+    # ===============================================================================================
+
+    @EdidProperty
     def manufacture_year(self):
-        class ManufactureYear:
-
-            def __init__(self, value):
-                self._value = value
-
-            @property
-            def value(self):
-                return self._value
-
-            @property
-            def bytes(self):
-                return (self._value - 1990).to_bytes()
-
-            @property
-            def byte_range(self):
-                return [17,18]
-
-        return ManufactureYear(self._manufacture_year)
+        return self._manufacture_year
 
     @manufacture_year.setter
     def manufacture_year(self, value):
         self._manufacture_year = value
 
-    @property
+    @manufacture_year.byte_converter
+    def manufacture_year(value):
+        return (value - 1990).to_bytes()
+
+    product_code.byte_range = [17,18]
+
+    # ===============================================================================================
+
+    @EdidProperty
     def edid_version(self):
-        class EdidVersion:
-
-            def __init__(self, value):
-                self._value = value
-
-            @property
-            def value(self):
-                return self._value
-
-            @property
-            def bytes(self):
-                return bytes([int(x) for x in self._value.split('.')])
-
-            @property
-            def byte_range(self):
-                return [18,20]
-
-        return EdidVersion(self._edid_version)
+        return self._edid_version
 
     @edid_version.setter
     def edid_version(self, value):
         self._edid_version = value
 
+    @edid_version.byte_converter
+    def edid_version(value):
+        return bytes([int(x) for x in value.split('.')])
+
+    product_code.byte_range = [19,20]
+
+    # ===============================================================================================
+
     @property
-    def bytes(self):
+    def as_bytes(self):
         return (
             bytes.fromhex(self.HEADER_PATTERN)
-            + self.manufacturer_id.bytes
-            + self.product_code.bytes
-            + self.serial_num.bytes
-            + self.manufacture_week.bytes
-            + self.manufacture_year.bytes
-            + self.edid_version.bytes
+            + self.manufacturer_id.as_bytes
+            + self.product_code.as_bytes
+            + self.serial_num.as_bytes
+            + self.manufacture_week.as_bytes
+            + self.manufacture_year.as_bytes
+            + self.edid_version.as_bytes
             )
 
     @property
@@ -258,7 +253,8 @@ class Header():
         return False
 
     def __str__(self):
-        return bytes_to_hex_block(self.bytes)
+        return bytes_to_hex_block(self.as_bytes)
+
 
 class EstablishedTiming():
     @property
@@ -282,7 +278,7 @@ header = Header(
     manufacture_week=5,
     manufacture_year=2012,
     edid_version='1.3'
-    )
+)
 
 simple_test(header, expected)
 
